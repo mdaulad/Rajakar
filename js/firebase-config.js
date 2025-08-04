a8998297dd33da1c81c92a0a8ce2fefac19988b3
mdaulad <safardhaka2020@gmail.com>
2025-08-04 23:43:03 +0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// তোমার Firebase কনফিগ
+const firebaseConfig = {
+  apiKey: "AIzaSyBXVRSfeiGz0scpyYN-q3zr5vGYu-vatFE",
+  authDomain: "rajakar-library.firebaseapp.com",
+  projectId: "rajakar-library",
+  storageBucket: "rajakar-library.appspot.com",
+  messagingSenderId: "986607989402",
+  appId: "1:986607989402:web:aec536c944bcfc9a4c2523",
+  measurementId: "G-NPK519GFCW"
+};
+
+// Initialize
+firebase.initializeApp(firebaseConfig);
